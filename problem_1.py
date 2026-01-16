5c05a258c09fa1526e9349cf9ce728b862c33c68
jojewar Dattatray <jojewardattatray@gmail.com>
2026-01-16 09:27:17 +0530

    return indices of the two numbers such that they add up to target

--- problem_1.py
+++ problem_1.py
@@ -1,3 +1,12 @@
+""" Problem : Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. 
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order """
+
+
+# Solution :
+
 class solution:
     def __init__(self):
         self.list = [2, 5, 5, 11]
@@ -13,6 +22,7 @@ class solution:
                     b = self.list[j]
                     if a + b == self.target:
                         print(i, j)
+                        return
                     else:
                         pass   
                 
